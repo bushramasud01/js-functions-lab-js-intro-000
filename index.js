bb783fb3f9b8a8864a3dad8686ab566f1ae95384
Bushra Masud <bushra.masud111@gmail.com>
2018-08-30 06:11:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function happyHolidays(holidays) {
 function happyHolidaysTo(Joe) {
   return ('Happy holidays, you!')
 }
+function happyHolidaysTo(holidays)&&(Joe){
+  return ( " Happy holidays, Joe!")
+}
